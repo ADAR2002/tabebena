1f431c7724f47bbdb6677f9c261245297c2a8421
Ahmad Alnajjar <ahmadalnajjar301@gmail.com>
2025-12-05 20:26:23 +0000

    edit memory

--- src/main.ts
+++ src/main.ts
@@ -40,7 +40,7 @@ async function bootstrap() {
   });
 
   if (!isVercel) {
-    await app.listen(port);
+    await app.listen(port,"0.0.0.0");
     // eslint-disable-next-line no-console
     console.log(`Application is running on: http://localhost:${port}`);
     // eslint-disable-next-line no-console
