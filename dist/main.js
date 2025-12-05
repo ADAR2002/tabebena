f152aa7fb829c42244ab2bafae0f804695b55b3d
Ahmad Alnajjar <ahmadalnajjar301@gmail.com>
2025-12-05 20:40:35 +0000

    edit memory

--- dist/main.js
+++ dist/main.js
@@ -34,7 +34,7 @@ async function bootstrap() {
         },
     });
     if (!isVercel) {
-        await app.listen(port);
+        await app.listen(port, "0.0.0.0");
         console.log(`Application is running on: http://localhost:${port}`);
         console.log(`Swagger documentation: http://localhost:${port}/api/docs`);
     }
